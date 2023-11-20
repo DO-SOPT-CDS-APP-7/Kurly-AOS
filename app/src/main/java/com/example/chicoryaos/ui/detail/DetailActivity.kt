936503cd14df67c9c60afade1fdd8d47bb777617
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-11-20 19:32:06 +0900

    #1[setting] detail activity 하단 버튼들 작업 및 button custom xml

--- app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
@@ -1,10 +1,30 @@
 package com.example.chicoryaos.ui.detail
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.example.chicoryaos.R
+import com.example.chicoryaos.databinding.ActivityDetailBinding
+import com.example.chicoryaos.util.binding.BindingActivity
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        initPurchaseBtnClickListener()
+        initBookmarkBtnClickListener()
+    }
+
+    private fun initBookmarkBtnClickListener() {
+        binding.ivDetailBookmark.setOnClickListener {
+            // TODO(북마크 클릭시)
+        }
+    }
+
+    private fun initPurchaseBtnClickListener() {
+        binding.btnDetailPurchase.setOnClickListener {
+            // TODO(구매하기 클릭시)
+        }
     }
 }
