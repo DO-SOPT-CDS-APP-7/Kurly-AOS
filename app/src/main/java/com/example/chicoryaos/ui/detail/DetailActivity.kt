1298520becd4898a98b1d8b7d54dfe988f901305
Hailey <91793891+stellar-halo@users.noreply.github.com>
2023-11-28 20:27:25 +0900

    #9 상하단으로 이동하는 플로팅 버튼 추가

--- app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.chicoryaos.ui.detail
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ScrollView
 import androidx.activity.viewModels
 import com.example.chicoryaos.R
 import com.example.chicoryaos.databinding.ActivityDetailBinding
@@ -22,6 +23,17 @@ class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_
     private fun initClickListener() {
         initPurchaseBtnClickListener()
         initBookmarkBtnClickListener()
+        initFabBtnClickListener()
+    }
+
+    private fun initFabBtnClickListener() {
+        val scroll = { focus: Int -> binding.scrollView.fullScroll(focus) }
+        binding.btnDetailFabUp.setOnClickListener {
+            scroll(ScrollView.FOCUS_UP)
+        }
+        binding.btnDetailFabDown.setOnClickListener {
+            scroll(ScrollView.FOCUS_DOWN)
+        }
     }
 
     private fun initBookmarkBtnClickListener() {
