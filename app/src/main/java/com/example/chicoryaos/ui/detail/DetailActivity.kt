2a8d66c21cb63044792b392fad4adf9636d389f7
river99 <qkrrkd42@naver.com>
2023-11-28 12:13:42 +0900

    #9 detail xml data연결

--- app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
@@ -1,13 +1,17 @@
 package com.example.chicoryaos.ui.detail
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.chicoryaos.R
 import com.example.chicoryaos.databinding.ActivityDetailBinding
 import com.example.chicoryaos.util.binding.BindingActivity
 
 class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+    private val viewModel: DetailViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.vm = viewModel
         initClickListener()
     }
 
