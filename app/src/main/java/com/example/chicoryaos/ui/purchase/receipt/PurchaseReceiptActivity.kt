b510fe93619a8e44286418398ac00dd66c87ac14
chanu <cw8881@naver.com>
2023-11-29 01:04:01 +0900

    #11 [feat] : 데이터 받아와서 mvvm구현

--- app/src/main/java/com/example/chicoryaos/ui/purchase/receipt/PurchaseReceiptActivity.kt
+++ app/src/main/java/com/example/chicoryaos/ui/purchase/receipt/PurchaseReceiptActivity.kt
@@ -1,18 +1,47 @@
 package com.example.chicoryaos.ui.purchase.receipt
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.chicoryaos.R
 import com.example.chicoryaos.databinding.ActivityPurchaseReceiptBinding
+import com.example.chicoryaos.model.PriceEntity
+import com.example.chicoryaos.ui.purchase.PurchaseBasketFragment
 import com.example.chicoryaos.util.binding.BindingActivity
+import com.example.chicoryaos.util.extensions.getParcelable
 
 class PurchaseReceiptActivity :
     BindingActivity<ActivityPurchaseReceiptBinding>(R.layout.activity_purchase_receipt) {
+
+    private val viewModel: PurchaseReceiptViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initDataBinding()
         initGetIntent()
+        initBackBtnClickListener()
+    }
+
+    private fun initDataBinding() {
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
     }
 
     private fun initGetIntent() {
+        val priceEntity =
+            intent.getParcelable<PriceEntity>(PurchaseBasketFragment.TAG, PriceEntity::class.java)
+
+        if (priceEntity != null) {
+            with(viewModel) {
+                count.value = priceEntity.count
+                setPurchasePrice(priceEntity.price)
+            }
+        }
+    }
+
+    private fun initBackBtnClickListener() {
+        binding.btnReceiptBack.setOnClickListener {
+            finish()
+        }
     }
 }
