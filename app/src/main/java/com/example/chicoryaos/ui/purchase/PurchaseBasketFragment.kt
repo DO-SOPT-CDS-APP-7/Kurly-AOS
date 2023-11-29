c2a539c70b6c5cd198bc5e49e9f4449141f7134b
chanu <cw8881@naver.com>
2023-11-30 04:09:00 +0900

    #14 [feat] : 장바구니 바텀시트 화면 서버 연결 및 entity 생성

--- app/src/main/java/com/example/chicoryaos/ui/purchase/PurchaseBasketFragment.kt
+++ app/src/main/java/com/example/chicoryaos/ui/purchase/PurchaseBasketFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import coil.load
 import com.example.chicoryaos.databinding.BottomSheetFragmentPurchaseBasketBinding
 import com.example.chicoryaos.model.PriceEntity
+import com.example.chicoryaos.model.ResponseProductDTO
+import com.example.chicoryaos.ui.detail.DetailViewModel
 import com.example.chicoryaos.ui.purchase.receipt.PurchaseReceiptActivity
 import com.example.chicoryaos.util.extensions.AnimateProgressBar
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -18,7 +22,18 @@ class PurchaseBasketFragment : BottomSheetDialogFragment() {
     private val binding: BottomSheetFragmentPurchaseBasketBinding
         get() = requireNotNull(_binding)
 
-    private val viewModel: PurchaseViewModel by activityViewModels()
+    private val purchaseViewModel by activityViewModels<PurchaseViewModel>()
+    private val detailViewModel by viewModels<DetailViewModel>()
+
+    private val productEntity = PriceEntity(
+        id = 1,
+        count = 0,
+        deliveryType = "",
+        productName = "",
+        originalPrice = 0,
+        discountedPrice = 0,
+        imageUrl = "",
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,37 +48,53 @@ class PurchaseBasketFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initDataBinding()
+        initGetDetailData()
         initProgressAnimation()
         initDirectBuyBtnClickListener()
     }
 
     private fun initDataBinding() {
         binding.lifecycleOwner = viewLifecycleOwner
-        binding.vm = viewModel
+        binding.vm = purchaseViewModel
+    }
+
+    private fun initGetDetailData() {
+        detailViewModel.produce.observe(viewLifecycleOwner) {
+            if (it != null) {
+                binding.ivBasketImg.load(it.imageURL)
+                setProductEntityDetailData(it)
+            }
+        }
+    }
+
+    private fun setProductEntityDetailData(it: ResponseProductDTO.Product) {
+        productEntity.deliveryType = it.deliveryType
+        productEntity.productName = it.productName
+        productEntity.originalPrice = it.originalPrice
+        productEntity.imageUrl = it.imageURL
     }
 
     private fun initProgressAnimation() {
         val progressBar = binding.pgBasket
-        val anim =
-            AnimateProgressBar(progressBar, 0f, (viewModel.purchaseProgress.value ?: 0).toFloat())
+        val anim = AnimateProgressBar(
+            progressBar,
+            0f,
+            (purchaseViewModel.purchaseProgress.value ?: 0).toFloat(),
+        )
         anim.duration = 1500
         progressBar.startAnimation(anim)
     }
 
     private fun initDirectBuyBtnClickListener() {
         binding.btnBasketBuy.setOnClickListener {
-            val priceEntity = createPriceEntity()
-            startPurchaseReceiptActivity(priceEntity)
+            setProductEntityPurchaseData()
+            startPurchaseReceiptActivity(productEntity)
         }
     }
 
-    private fun createPriceEntity(): PriceEntity {
-        return PriceEntity(
-            id = 1,
-            name = "상품명",
-            count = viewModel.count.value ?: 0,
-            price = viewModel.price,
-        )
+    private fun setProductEntityPurchaseData() {
+        productEntity.count = purchaseViewModel.count.value ?: 0
+        productEntity.discountedPrice = purchaseViewModel.price ?: 0
     }
 
     private fun startPurchaseReceiptActivity(priceEntity: PriceEntity) {
