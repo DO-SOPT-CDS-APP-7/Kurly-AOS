8619fe78563ecdc2647cdc758c8746e580b909d1
pump9918 <hoyeon9918@gmail.com>
2023-11-30 23:52:42 +0900

    #16 [feat] 찜하기 버튼 스낵바 적용

--- app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/chicoryaos/ui/detail/DetailActivity.kt
@@ -8,10 +8,12 @@ import androidx.activity.viewModels
 import com.example.chicoryaos.R
 import com.example.chicoryaos.databinding.ActivityDetailBinding
 import com.example.chicoryaos.ui.purchase.PurchaseFragment
+import com.example.chicoryaos.util.CustomBookmarkSnackbar
 import com.example.chicoryaos.util.binding.BindingActivity
 
 class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
     private val viewModel by viewModels<DetailViewModel>()
+    private var isBookmarked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +44,16 @@ class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_
         binding.ivDetailBookmark.setOnClickListener {
             val BookmarkBottomSheet = BookmarkFragment()
             BookmarkBottomSheet.show(supportFragmentManager, BookmarkFragment.TAG)
+
+            isBookmarked = !isBookmarked
+
+            if (isBookmarked) {
+                binding.ivDetailBookmark.setImageResource(R.drawable.ic_button_pressed_full)
+                CustomBookmarkSnackbar.showBookmarkAddSnackbar(binding.root, this)
+            } else {
+                binding.ivDetailBookmark.setImageResource(R.drawable.ic_button_pressed)
+                CustomBookmarkSnackbar.showBookmarkDeleteSnackbar(binding.root, this)
+            }
         }
     }
 
---
+++ app/src/main/java/com/example/chicoryaos/util/extensions/CumstomSnackbar.kt
@@ -0,0 +1,61 @@
+package com.example.chicoryaos.util
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.example.chicoryaos.R
+import com.google.android.material.snackbar.Snackbar
+
+class CustomBookmarkSnackbar {
+
+    companion object {
+
+        fun showBookmarkAddSnackbar(anchorView: View, context: Context) {
+            showSnackbar(
+                anchorView,
+                context,
+                R.layout.snackbar_bookmark_add,
+            )
+        }
+
+        fun showBookmarkDeleteSnackbar(anchorView: View, context: Context) {
+            showSnackbar(
+                anchorView,
+                context,
+                R.layout.snackbar_bookmark_delete,
+            )
+        }
+
+        private fun showSnackbar(
+            anchorView: View,
+            context: Context,
+            layoutId: Int,
+        ) {
+            val customSnackbarView = LayoutInflater.from(context).inflate(layoutId, null)
+            val snackbar = Snackbar.make(anchorView, "", Snackbar.LENGTH_LONG)
+            val snackbarLayout = snackbar.view as Snackbar.SnackbarLayout
+
+            val params = snackbarLayout.layoutParams as ViewGroup.MarginLayoutParams
+            params.setMargins(0, 0, 0, 50)
+            snackbarLayout.layoutParams = params
+
+            snackbarLayout.setBackgroundColor(
+                ContextCompat.getColor(
+                    context,
+                    android.R.color.transparent,
+                ),
+            )
+
+            val customParams = ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+            )
+            snackbarLayout.addView(customSnackbarView, 0, customParams)
+
+            snackbar.show()
+        }
+    }
+}
