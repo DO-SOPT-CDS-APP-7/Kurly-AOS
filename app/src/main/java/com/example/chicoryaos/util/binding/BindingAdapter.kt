ef2257b5e510f7fe6c3467dcff0e7d2fc4daf8d8
pump9918 <hoyeon9918@gmail.com>
2023-11-30 23:30:08 +0900

    #16 [feat] BindingAdapter Coil 이미지, 가격 포매터 적용

--- app/src/main/java/com/example/chicoryaos/util/binding/BindingAdapter.kt
+++ app/src/main/java/com/example/chicoryaos/util/binding/BindingAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.chicoryaos.util.binding
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import coil.decode.SvgDecoder
+import coil.load
 import com.bumptech.glide.Glide
+import com.example.chicoryaos.util.extensions.PriceFormatter
 
 object BindingAdapter {
     @BindingAdapter("setImage")
@@ -12,6 +16,28 @@ object BindingAdapter {
             Glide.with(context)
                 .load(imgUrl)
                 .into(this)
+        }
+    }
+
+    // 중복시 아래 삭제 가능
+    @BindingAdapter("setCoilImage")
+    @JvmStatic
+    fun ImageView.setCoilImage(imgUrl: String?) {
+        this.let {
+            it.load(imgUrl) {
+                if (imgUrl?.endsWith(".svg") == true) {
+                    decoderFactory { result, options, _ -> SvgDecoder(result.source, options) }
+                }
+                crossfade(true)
+            }
+        }
+    }
+
+    @BindingAdapter("formattedPrice")
+    @JvmStatic
+    fun TextView.setFormattedPrice(amount: Int) {
+        this.let {
+            it.text = "${PriceFormatter.formatPrice(amount)}원"
         }
     }
 }
