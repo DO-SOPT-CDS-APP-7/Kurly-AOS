eaa79bcb2ac637bdbbab71e428eba4fc98e34172
pump9918 <hoyeon9918@gmail.com>
2023-11-30 23:23:16 +0900

    #16 [feat] build.gradle설정 및 바텀시트 xml, snackbar 추가

--- app/src/main/java/com/example/chicoryaos/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/example/chicoryaos/ui/bookmark/BookmarkFragment.kt
@@ -2,42 +2,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import com.example.chicoryaos.databinding.BottomSheetFragmentBookmarkBinding
-import com.example.chicoryaos.ui.bookmark.BookmarkAdapter
-import com.example.chicoryaos.ui.bookmark.BookmarkViewModel
+import com.example.chicoryaos.databinding.BottomSheetFragmentBookmarkBasketBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BookmarkFragment : BottomSheetDialogFragment() {
 
-    private var _binding: BottomSheetFragmentBookmarkBinding? = null
-    private val binding: BottomSheetFragmentBookmarkBinding
+    private var _binding: BottomSheetFragmentBookmarkBasketBinding? = null
+    private val binding: BottomSheetFragmentBookmarkBasketBinding
         get() = requireNotNull(_binding)
-
-    private lateinit var viewModel: BookmarkViewModel
-    private lateinit var bookmarkAdapter: BookmarkAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = BottomSheetFragmentBookmarkBinding.inflate(inflater, container, false)
+        _binding = BottomSheetFragmentBookmarkBasketBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewModel = ViewModelProvider(this).get(BookmarkViewModel::class.java)
-        bookmarkAdapter = BookmarkAdapter(requireContext())
-
-        binding.rvWishListOtherConsumerItem.adapter = bookmarkAdapter
-        binding.rvWishListPurchaseWithItem.adapter = bookmarkAdapter
-
-        viewModel.bookmarkList.observe(viewLifecycleOwner, { bookmarkList ->
-            bookmarkAdapter.setBookmarkList(bookmarkList)
-        })
     }
 
     companion object {
