c7b629109a780f2ac5d0e70cd7a6d86fa57a290a
pump9918 <hoyeon9918@gmail.com>
2023-11-30 23:48:35 +0900

    #16 [delete] 사용하지 않는 bookmark 더미데이터 삭제

--- app/src/main/java/com/example/chicoryaos/ui/bookmark/Bookmark.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chicoryaos.ui.bookmark
-
-import androidx.annotation.DrawableRes
-
-data class Bookmark(
-    @DrawableRes val productImage: Int,
-    val productTitle: String,
-    val productPrice: Int,
-)
