042410975d0962a870cd6b722187fb753d455bf3
pump9918 <hoyeon9918@gmail.com>
2023-11-30 23:26:48 +0900

    #16 [feat] 관련상품, 추천상품 get interface 함수 생성

--- app/src/main/java/com/example/chicoryaos/data/AuthService.kt
+++ app/src/main/java/com/example/chicoryaos/data/AuthService.kt
@@ -1,11 +1,29 @@
 package com.example.chicoryaos.data
 
 import com.example.chicoryaos.model.ResponseProductDTO
+import com.example.chicoryaos.model.ResponseRecommendBookmarkDTO
+import com.example.chicoryaos.model.ResponseRelatedBookmarkDTO
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface AuthService {
     @GET("product/{productId}")
     fun getProduct(@Path("productId") productId: Int): Call<ResponseProductDTO>
+
+    @GET("/product/{productId}/related")
+    suspend fun getRelatedProduct(
+        @Path("productId") productId: Int,
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+    ): Response<ResponseRelatedBookmarkDTO>
+
+    @GET("/product/{productId}/recommend")
+    suspend fun getRecommendProduct(
+        @Path("productId") productId: Int,
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+    ): Response<ResponseRecommendBookmarkDTO>
 }
