8b45ab7acd7bc44489d65f3373e2d67304cf47a7
pump9918 <hoyeon9918@gmail.com>
2023-12-01 02:44:44 +0900

    #16 [fix] 스낵바 수정

--- app/src/main/java/com/example/chicoryaos/util/extensions/CumstomSnackbar.kt
+++ app/src/main/java/com/example/chicoryaos/util/extensions/CumstomSnackbar.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import com.example.chicoryaos.R
 import com.google.android.material.snackbar.Snackbar
@@ -58,6 +57,5 @@ class CustomBookmarkSnackbar {
 
             snackbar.show()
         }
-
     }
 }
