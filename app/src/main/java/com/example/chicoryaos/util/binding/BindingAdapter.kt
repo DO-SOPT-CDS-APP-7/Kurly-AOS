bc47b2474140c890f43cf09a83f5fa859c594f08
river99 <qkrrkd42@naver.com>
2023-12-01 14:41:13 +0900

    #19 int로 받아오면 끊어서 format해주는 setFormattedPrice 추가(ft. 찬우)

--- app/src/main/java/com/example/chicoryaos/util/binding/BindingAdapter.kt
+++ app/src/main/java/com/example/chicoryaos/util/binding/BindingAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.chicoryaos.util.binding
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.chicoryaos.util.extensions.PriceFormatter
 
 object BindingAdapter {
     @BindingAdapter("setImage")
@@ -12,6 +14,14 @@ object BindingAdapter {
             Glide.with(context)
                 .load(imgUrl)
                 .into(this)
+        }
+    }
+
+    @BindingAdapter("formattedPrice")
+    @JvmStatic
+    fun TextView.setFormattedPrice(amount: Int) {
+        this.let {
+            it.text = "${PriceFormatter.formatPrice(amount)}원"
         }
     }
 }
