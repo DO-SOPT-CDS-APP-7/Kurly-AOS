692756f5e555883d57d15e71822097b9af03cfad
river99 <qkrrkd42@naver.com>
2023-12-01 14:41:25 +0900

    #19 server 통신 함수 작성

--- app/src/main/java/com/example/chicoryaos/data/AuthService.kt
+++ app/src/main/java/com/example/chicoryaos/data/AuthService.kt
@@ -1,11 +1,21 @@
 package com.example.chicoryaos.data
 
 import com.example.chicoryaos.model.ResponseProductDTO
+import com.example.chicoryaos.model.ResponseRelatedDTO
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface AuthService {
     @GET("product/{productId}")
     fun getProduct(@Path("productId") productId: Int): Call<ResponseProductDTO>
+
+    @GET("/product/{productId}/related")
+    suspend fun getRelatedProduct(
+        @Path("productId") productId: Int,
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+    ): Response<ResponseRelatedDTO>
 }
